c444e9db4b2988225d397b007ddc8bcbc1de05ff
Joeltom-PT <joeltompt@gmail.com>
2025-03-14 20:07:55 +0530

    fix: resolve zenscroll module issue by including it in optimizeDeps

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 9001
-  },
-  base: './', 
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+  optimizeDeps: {
+    include: ['zenscroll']
   }
 })
