749073ebe7ba2c7d3f47e741eb8197d63dd8a929
Joeltom-PT <joeltompt@gmail.com>
2025-03-14 20:05:41 +0530

    fix: update Vite config and Vercel routing to resolve asset loading issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: "./"
+  server: {
+    port: 9001
+  },
+  base: './', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
