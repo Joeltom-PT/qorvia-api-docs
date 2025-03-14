3c000d6eac37af562f6fe56293916528707b6cc1
Joeltom-PT <joeltompt@gmail.com>
2025-03-14 20:11:00 +0530

    fix: exclude zenscroll from optimization and allow CommonJS dependencies in build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['zenscroll']
+    exclude: ['zenscroll']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
   }
 })
